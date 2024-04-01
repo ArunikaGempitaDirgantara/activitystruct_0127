1de6b75a038c84866224f79673c6e84eb6edd830
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-04-01 18:29:48 +0700

    Mengubah Type Data Umur di Bagian Struct Mahasiswa

--- Struct.cpp
+++ Struct.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
 struct Mahasiswa {
     string nim;
     string nama;
     string alamat;
-    string umur;
+    int umur;
 };
 
 int main()
